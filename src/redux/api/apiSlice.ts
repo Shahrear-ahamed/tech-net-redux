378abfd04a3894acc8cf910e4525b386752384ee
Shahrear-ahamed <its.shahrear@gmail.com>
2023-07-12 16:43:43 +0600

    [DONE] product comment section add and learn for mutation and option for dynamic data load

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -5,6 +5,7 @@ export const productsApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:5000/',
   }),
+  tagTypes: ['comments'],
   endpoints: (builder) => ({
     getProduct: builder.query({
       query: () => '/products',
@@ -12,7 +13,24 @@ export const productsApi = createApi({
     singleProduct: builder.query({
       query: (id) => `/product/${id}`,
     }),
+    postComment: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/comment/${id}`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['comments'],
+    }),
+    getComments: builder.query({
+      query: (id) => `/comment/${id}`,
+      providesTags: ['comments'],
+    }),
   }),
 });
 
-export const { useGetProductQuery, useSingleProductQuery } = productsApi;
+export const {
+  useGetProductQuery,
+  useSingleProductQuery,
+  usePostCommentMutation,
+  useGetCommentsQuery,
+} = productsApi;
